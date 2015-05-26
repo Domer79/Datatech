d1c8071414e7c24ef5cb8a8da77b44cbb9cbabba
Domer79 <garipovd@mail.ru>
2015-05-26 15:38:08 +0500

    3333

--- Test.Wpf/Interfaces/IState.cs
+++ Test.Wpf/Interfaces/IState.cs
@@ -1,8 +1,11 @@
+using System;
+
 namespace Test.Wpf.Interfaces
 {
     public interface IState
     {
         string CurrentState { get; set; }
+        Action 
         string NextState { get; set; }
         int GetHashCode();
         bool Equals(object compareTo);
